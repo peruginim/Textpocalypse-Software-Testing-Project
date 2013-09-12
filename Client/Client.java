a149723f1a0c370204310c75705673d68c320d6c
rbennett91 <rbennett91@1f072946-bd86-44d2-8cb3-37d1c841b419>
2013-09-12 02:57:02 +0000

    Added basic Server class, proved basic communication between client server, added 'make run' rule to makefiles

--- Client/Client.java
+++ Client/Client.java
@@ -5,131 +5,157 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
+import java.io.*;
+import java.net.*;
 
 public class Client implements ActionListener
 {
- 
- public static String currentUser;
 
- //Client Frame
- JFrame clientFrame;
- JPanel loginPanel, serverPanel, emptyPanel;
- 
- //Login Panel
- JPanel loginCredPanel;
- JButton newUserButton, loginButton;
- JLabel loginIdText, passwordText;
- JTextField usernameTextBox;
- JPasswordField passwordTextBox;
- JButton exitButton;
- 
- //Server Panel
- JLabel serverStatus;
- 
- 
- public static void main(String[] args)
- {
-  new Client();
- }
- 
- public Client()
- {
-  createButtons();
-  createLabels();
-  createPanels();
-  createFrame();
- }
- 
- public String getUser()
- {
-	 return currentUser;
- }
- 
- public void setUser(String user)
- {
-	 currentUser = user;
- }
- 
- public void createFrame()
- {
-  clientFrame = new JFrame("Textpocalypse: Purdue -- LOGIN");
-  clientFrame.setLayout(new BorderLayout());
-  clientFrame.setSize(500, 200);
-  
-  clientFrame.add(loginPanel, BorderLayout.CENTER);
-  clientFrame.add(serverPanel, BorderLayout.EAST);
-  
-  clientFrame.setResizable(false);
-  clientFrame.setLocationRelativeTo(null);
-  clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  clientFrame.setVisible(true);
- }
- 
- public void createPanels()
- {
-  //LoginCreditialsPanel
-  loginCredPanel = new JPanel(new GridLayout(2,2));
-  loginCredPanel.add(loginIdText);
-  loginCredPanel.add(usernameTextBox);
-  loginCredPanel.add(passwordText);
-  loginCredPanel.add(passwordTextBox);
-  
-  //EmtpyPanel
-  emptyPanel = new JPanel(new GridLayout(1,1));
-  
-  //LoginPanel
-  loginPanel = new JPanel(new GridLayout(5, 1));
-  loginPanel.add(loginCredPanel);
-  loginPanel.add(loginButton);
-  loginPanel.add(newUserButton);
-  loginPanel.add(emptyPanel);
-  loginPanel.add(exitButton);
-  
-  //ServerPanel
-  serverPanel = new JPanel(new BorderLayout());
-  serverPanel.add(serverStatus, BorderLayout.CENTER);
- 
- }
- 
- public void createButtons()
- {
-  newUserButton = new JButton("Create New User");
-  newUserButton.addActionListener(this);
-  
-  loginButton = new JButton("Enter");
-  loginButton.addActionListener(this);
-  
-  exitButton = new JButton("Exit Game");
-  exitButton.addActionListener(this);
- }
- 
- public void createLabels()
- {
-  loginIdText = new JLabel("Login ID:");
-  passwordText = new JLabel("Password:");
-  serverStatus = new JLabel("Server stuff will go here");
-  usernameTextBox = new JTextField();
-  passwordTextBox = new JPasswordField();
- }
- 
- public void actionPerformed(ActionEvent e)
- {
-  Object pressed = e.getSource();
-  
-  if(pressed.equals(newUserButton))
-  {
-   //Go to new user screen. Hide current frame
-   new CreateNewUser();
-   clientFrame.dispose();
-  }else if(pressed.equals(loginButton))
-  {
-   //Communicate with server to login
-   //Change currentUser to logged in User
-  }else if(pressed.equals(exitButton))
-  {
-   System.exit(1);
-  }
- }
+	public static String currentUser;
+
+	//Client Frame
+	JFrame clientFrame;
+	JPanel loginPanel, serverPanel, emptyPanel;
+
+	//Login Panel
+	JPanel loginCredPanel;
+	JButton newUserButton, loginButton;
+	JLabel loginIdText, passwordText;
+	JTextField usernameTextBox;
+	JPasswordField passwordTextBox;
+	JButton exitButton;
+
+	//Server Panel
+	JLabel serverStatus;
+
+
+	public static void main(String[] args)
+	{
+		new Client();
+	}
+
+	public Client()
+	{
+		createButtons();
+		createLabels();
+		createPanels();
+		createFrame();
+		try
+		{ 
+			if ( connectToServer() ) serverStatus.setText("Server is running, better catch it!"); 
+		}
+		catch (Exception e)
+		{
+			serverStatus.setText("Exception! Error with server...");
+		}
+
+	}
+
+	public String getUser()
+	{
+		return currentUser;
+	}
+
+	public void setUser(String user)
+	{
+		currentUser = user;
+	}
+
+	public void createFrame()
+	{
+		clientFrame = new JFrame("Textpocalypse: Purdue -- LOGIN");
+		clientFrame.setLayout(new BorderLayout());
+		clientFrame.setSize(500, 200);
+
+		clientFrame.add(loginPanel, BorderLayout.CENTER);
+		clientFrame.add(serverPanel, BorderLayout.EAST);
+
+		clientFrame.setResizable(false);
+		clientFrame.setLocationRelativeTo(null);
+		clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		clientFrame.setVisible(true);
+	}
+
+	public void createPanels()
+	{
+		//LoginCreditialsPanel
+		loginCredPanel = new JPanel(new GridLayout(2,2));
+		loginCredPanel.add(loginIdText);
+		loginCredPanel.add(usernameTextBox);
+		loginCredPanel.add(passwordText);
+		loginCredPanel.add(passwordTextBox);
+
+		//EmtpyPanel
+		emptyPanel = new JPanel(new GridLayout(1,1));
+
+		//LoginPanel
+		loginPanel = new JPanel(new GridLayout(5, 1));
+		loginPanel.add(loginCredPanel);
+		loginPanel.add(loginButton);
+		loginPanel.add(newUserButton);
+		loginPanel.add(emptyPanel);
+		loginPanel.add(exitButton);
+
+		//ServerPanel
+		serverPanel = new JPanel(new BorderLayout());
+		serverPanel.add(serverStatus, BorderLayout.CENTER);
+
+	}
+
+	public void createButtons()
+	{
+		newUserButton = new JButton("Create New User");
+		newUserButton.addActionListener(this);
+
+		loginButton = new JButton("Enter");
+		loginButton.addActionListener(this);
+
+		exitButton = new JButton("Exit Game");
+		exitButton.addActionListener(this);
+	}
+
+	public void createLabels()
+	{
+		loginIdText = new JLabel("Login ID:");
+		passwordText = new JLabel("Password:");
+		serverStatus = new JLabel("Server stuff will go here");
+		usernameTextBox = new JTextField();
+		passwordTextBox = new JPasswordField();
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		Object pressed = e.getSource();
+
+		if(pressed.equals(newUserButton))
+		{
+			//Go to new user screen. Hide current frame
+			new CreateNewUser();
+			clientFrame.dispose();
+		}else if(pressed.equals(loginButton))
+		{
+			//Communicate with server to login
+			//Change currentUser to logged in User
+		}else if(pressed.equals(exitButton))
+		{
+			System.exit(1);
+		}
+	}
+
+	public boolean connectToServer() throws Exception
+	{
+		String receivedSentence;
+		Socket clientSocket = new Socket("localhost", 4444);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		outToServer.writeBytes("Hello, I am the client!\n");
+		receivedSentence = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + receivedSentence);
+		clientSocket.close();
+		return true;
+	}
+
+
 }
 
 
@@ -138,7 +164,7 @@ public class Client implements ActionListener
  *
  * @CreateNewUser creates a new form allowing the user to 
  *  enter a desired username and password for character
- * creation. This class will send user data to the server
+ *  creation. This class will send user data to the server
  *  which will verify the uniqueness of the username.
  *  
  */
@@ -146,129 +172,129 @@ public class Client implements ActionListener
 class CreateNewUser implements ActionListener
 {
 
-	
- Client client = new Client();
- JFrame createUserFrame;
 
- //createUserPanel
- JPanel createUserPanel; 
- JButton createUserButton, backButton;
- JLabel usernameText;
- JLabel passwordText;
- JLabel password2Text;
- JTextField usernameTextBox;
- JPasswordField passwordTextBox;
- JPasswordField password2TextBox;
+	Client client = new Client();
+	JFrame createUserFrame;
 
- //textPanel
- JPanel textPanel;
- JTextField verificationText;
+	//createUserPanel
+	JPanel createUserPanel; 
+	JButton createUserButton, backButton;
+	JLabel usernameText;
+	JLabel passwordText;
+	JLabel password2Text;
+	JTextField usernameTextBox;
+	JPasswordField passwordTextBox;
+	JPasswordField password2TextBox;
 
- public static void main(String[] args)
- {
-  new CreateNewUser();
- }
- 
- public CreateNewUser()
- {
-  //stops extra boxs
-  client.clientFrame.dispose();
-  createButtons();
-  createLabels();
-  createPanels();
-  createFrame();
- }
- 
- public void createFrame()
- {
-  createUserFrame = new JFrame("Textpocalypse: Purdue -- CREATE NEW USER");
-  createUserFrame.setLayout(new BorderLayout());
-  createUserFrame.setSize(500, 200);
-  
-  createUserFrame.add(createUserPanel, BorderLayout.CENTER);
-  createUserFrame.add(textPanel, BorderLayout.SOUTH);
-  
-  createUserFrame.setResizable(false);
-  createUserFrame.setLocationRelativeTo(null);
-  createUserFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  createUserFrame.setVisible(true);
- }
- 
- public void createPanels()
- {
-  //createUser Panel
-  createUserPanel = new JPanel(new GridLayout(9, 1));
-  createUserPanel.add(usernameText);
-  createUserPanel.add(usernameTextBox);
-  createUserPanel.add(passwordText);
-  createUserPanel.add(passwordTextBox);
-  createUserPanel.add(password2Text);
-  createUserPanel.add(password2TextBox);
-  createUserPanel.add(createUserButton);
-  createUserPanel.add(backButton);
-  createUserPanel.add(verificationText);
+	//textPanel
+	JPanel textPanel;
+	JTextField verificationText;
 
-  //textPanel
-  textPanel = new JPanel(new GridLayout(1,1));
-  textPanel.add(verificationText);
+	public static void main(String[] args)
+	{
+		new CreateNewUser();
+	}
 
- }
+	public CreateNewUser()
+	{
+		//stops extra boxs
+		client.clientFrame.dispose();
+		createButtons();
+		createLabels();
+		createPanels();
+		createFrame();
+	}
 
- public void createButtons()
- {
-  createUserButton = new JButton("Create New User and Login");
-  createUserButton.addActionListener(this);
+	public void createFrame()
+	{
+		createUserFrame = new JFrame("Textpocalypse: Purdue -- CREATE NEW USER");
+		createUserFrame.setLayout(new BorderLayout());
+		createUserFrame.setSize(500, 200);
 
-  backButton = new JButton("Back");
-  backButton.addActionListener(this);
- }
- 
- public void createLabels()
- {
-  usernameText = new JLabel("Desired Username:");
-  passwordText = new JLabel("Password:");
-  password2Text = new JLabel("Re-enter password:");
-  usernameTextBox = new JTextField();
-  passwordTextBox = new JPasswordField();
-  password2TextBox = new JPasswordField();
-  verificationText = new JTextField();
-  verificationText.setText("Confirm or deny user creation in this textbox");
-  verificationText.setEditable(false);
- }
- 
- public void actionPerformed(ActionEvent e)
- {
-  Object pressed = e.getSource();
-  
-  if(pressed.equals(createUserButton))
-  {
-   //Create user and login
+		createUserFrame.add(createUserPanel, BorderLayout.CENTER);
+		createUserFrame.add(textPanel, BorderLayout.SOUTH);
 
-   //Check if passwords match
-   char[] temp = passwordTextBox.getPassword();
-   char[] temp2 = password2TextBox.getPassword();
-   String user = usernameTextBox.getText();
-   String pass = new String(temp);
-   String pass2 = new String(temp2);
-   if(!(pass.equals(pass2)))
-   {
-    verificationText.setText("The passwords you entered do not match");
-   }else
-   {
-    //Check if user is available with the server
-    //if yes go into main menu and make currentUser the created User
-	   client.setUser(user);
-	   new MainMenu();
-	   createUserFrame.dispose();
-	//if no update verificationText telling that the name is taken
-   }
+		createUserFrame.setResizable(false);
+		createUserFrame.setLocationRelativeTo(null);
+		createUserFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		createUserFrame.setVisible(true);
+	}
 
-  }else if(pressed.equals(backButton))
-  {
-   createUserFrame.dispose();
-   new Client();
-  }
- }
+	public void createPanels()
+	{
+		//createUser Panel
+		createUserPanel = new JPanel(new GridLayout(9, 1));
+		createUserPanel.add(usernameText);
+		createUserPanel.add(usernameTextBox);
+		createUserPanel.add(passwordText);
+		createUserPanel.add(passwordTextBox);
+		createUserPanel.add(password2Text);
+		createUserPanel.add(password2TextBox);
+		createUserPanel.add(createUserButton);
+		createUserPanel.add(backButton);
+		createUserPanel.add(verificationText);
+
+		//textPanel
+		textPanel = new JPanel(new GridLayout(1,1));
+		textPanel.add(verificationText);
+
+	}
+
+	public void createButtons()
+	{
+		createUserButton = new JButton("Create New User and Login");
+		createUserButton.addActionListener(this);
+
+		backButton = new JButton("Back");
+		backButton.addActionListener(this);
+	}
+
+	public void createLabels()
+	{
+		usernameText = new JLabel("Desired Username:");
+		passwordText = new JLabel("Password:");
+		password2Text = new JLabel("Re-enter password:");
+		usernameTextBox = new JTextField();
+		passwordTextBox = new JPasswordField();
+		password2TextBox = new JPasswordField();
+		verificationText = new JTextField();
+		verificationText.setText("Confirm or deny user creation in this textbox");
+		verificationText.setEditable(false);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		Object pressed = e.getSource();
+
+		if(pressed.equals(createUserButton))
+		{
+			//Create user and login
+
+			//Check if passwords match
+			char[] temp = passwordTextBox.getPassword();
+			char[] temp2 = password2TextBox.getPassword();
+			String user = usernameTextBox.getText();
+			String pass = new String(temp);
+			String pass2 = new String(temp2);
+			if(!(pass.equals(pass2)))
+			{
+				verificationText.setText("The passwords you entered do not match");
+			}else
+			{
+				//Check if user is available with the server
+				//if yes go into main menu and make currentUser the created User
+				client.setUser(user);
+				new MainMenu();
+				createUserFrame.dispose();
+				//if no update verificationText telling that the name is taken
+			}
+
+		}else if(pressed.equals(backButton))
+		{
+			createUserFrame.dispose();
+			new Client();
+		}
+	}
 
 
 }
@@ -284,19 +310,19 @@ class MainMenu implements ActionListener
 {
 	//This may be sloppy keeping it for now
 	Client client = new Client();
-	
+
 	JFrame mainMenuFrame;
-	
+
 	//MainMenuFrame
 	JButton instructButton, enterButton, logoutButton;
 	JLabel currentUserLabel;
 	JPanel buttonPanel;
-	
+
 	public static void main(String[] args)
 	{
 		new MainMenu();
 	}
-	
+
 	public MainMenu()
 	{
 		client.clientFrame.dispose();
@@ -305,23 +331,23 @@ class MainMenu implements ActionListener
 		createPanels();
 		createFrame();
 	}
-	
+
 	public void createFrame()
 	{
 		mainMenuFrame = new JFrame("Textpocalypse: Purdue -- MAIN MENU");
 		mainMenuFrame.setLayout(new GridLayout(1,2));
 		mainMenuFrame.setSize(500, 200);
-		
+
 		mainMenuFrame.add(buttonPanel);
 		mainMenuFrame.add(currentUserLabel);
-		
+
 		mainMenuFrame.setResizable(false);
 		mainMenuFrame.setLocationRelativeTo(null);
 		mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainMenuFrame.setVisible(true);
-		
+
 	}
-	
+
 	public void createPanels()
 	{
 		buttonPanel = new JPanel(new GridLayout(3,1));
@@ -329,25 +355,25 @@ class MainMenu implements ActionListener
 		buttonPanel.add(enterButton);
 		buttonPanel.add(logoutButton);
 	}
-	
+
 	public void createLabels()
 	{
 		String currentUser = client.getUser();
 		currentUserLabel = new JLabel("Current User:  " + currentUser);
 	}
-	
+
 	public void createButtons()
 	{
 		instructButton = new JButton("Instructions");
 		instructButton.addActionListener(this);
-		
+
 		enterButton = new JButton("Enter World");
 		enterButton.addActionListener(this);
-		
+
 		logoutButton = new JButton("Logout");
 		logoutButton.addActionListener(this);
 	}
-	
+
 	public void actionPerformed(ActionEvent e)
 	{
 		Object pressed = e.getSource();
@@ -379,49 +405,49 @@ class MainMenu implements ActionListener
 class Instructions implements ActionListener
 {
 	JFrame instructFrame;
-	
+
 	//Instructions Frame
 	JButton exitButton;
 	JLabel howtoPlayText;
-	
+
 	public static void main(String[] args)
 	{
 		new Instructions();
 	}
-	
+
 	Instructions()
 	{	
 		createButtons();
 		createLabels();
 		createFrame();
 	}
-	
+
 	public void createFrame()
 	{
 		instructFrame = new JFrame("Textpocalypse: Purdue -- INSTRUCTIONS");
 		instructFrame.setLayout(new GridLayout(2,1));
 		instructFrame.setSize(500, 200);
-		
+
 		instructFrame.add(howtoPlayText);
 		instructFrame.add(exitButton);
-		
+
 		instructFrame.setResizable(false);
 		instructFrame.setLocationRelativeTo(null);
 		instructFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		instructFrame.setVisible(true);
 	}
-	
+
 	public void createLabels()
 	{
 		howtoPlayText = new JLabel("A bunch of shit");
 	}
-	
+
 	public void createButtons()
 	{
 		exitButton = new JButton("Back");
 		exitButton.addActionListener(this);
 	}
-	
+
 	public void actionPerformed(ActionEvent e)
 	{
 		Object pressed = e.getSource();
@@ -440,30 +466,30 @@ class Instructions implements ActionListener
 class Game implements ActionListener
 {
 	Client client = new Client();
-	
+
 	JFrame gameFrame;
-	
+
 	//GameFrame
 	JPanel commandPanel, mapPanel, chatPanel, buttonPanel;
-	
+
 	//CommandPanel
 	//?
-	
+
 	//MapPanel
 	//?
-	
+
 	//chatPanel
 	//?
-	
+
 	//ButtonPanel
 	JLabel currentUserLabel;
 	JButton instructButton, logoutButton, exitButton;
-	
+
 	public static void main(String[] args)
 	{
 		new Game();
 	}
-	
+
 	public Game()
 	{
 		client.clientFrame.dispose();
@@ -472,60 +498,60 @@ class Game implements ActionListener
 		createPanels();
 		createFrame();
 	}
-	
+
 	public void createFrame()
 	{
 		gameFrame = new JFrame("Textpocalypse: Purdue -- GAME");
 		gameFrame.setLayout(new GridLayout(2,2));
 		gameFrame.setSize(750, 750);
-		
+
 		gameFrame.add(commandPanel);
 		gameFrame.add(mapPanel);
 		gameFrame.add(chatPanel);
 		gameFrame.add(buttonPanel);
-		
+
 		gameFrame.setResizable(false);
 		gameFrame.setLocationRelativeTo(null);
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameFrame.setVisible(true);
 	}
-	
+
 	public void createPanels()
 	{
 		//This will be decided Later
 		commandPanel = new JPanel(new BorderLayout());
-		
+
 		//This will be decided Later
 		mapPanel = new JPanel(new BorderLayout());
-		
+
 		//This will be decided Later
 		chatPanel = new JPanel(new BorderLayout());
-		
+
 		buttonPanel = new JPanel(new GridLayout(4,1));
 		buttonPanel.add(currentUserLabel);
 		buttonPanel.add(instructButton);
 		buttonPanel.add(logoutButton);
 		buttonPanel.add(exitButton);
 	}
-	
+
 	public void createLabels()
 	{
 		String currentUser = client.getUser();
 		currentUserLabel = new JLabel("Current User:  " + currentUser);
 	}
-	
+
 	public void createButtons()
 	{
 		instructButton = new JButton("Instructions");
 		instructButton.addActionListener(this);
-		
+
 		logoutButton = new JButton("Logout");
 		logoutButton.addActionListener(this);
-		
+
 		exitButton = new JButton("Exit Game");
 		exitButton.addActionListener(this);
 	}
-	
+
 	public void actionPerformed(ActionEvent e)
 	{
 		Object pressed = e.getSource();
@@ -542,5 +568,5 @@ class Game implements ActionListener
 			System.exit(1);
 		}
 	}
-	
+
 }
