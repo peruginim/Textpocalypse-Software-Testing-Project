e1b23446068844b45e7685a4f043fd7f82f452b7
rbennett91 <rbennett91@1f072946-bd86-44d2-8cb3-37d1c841b419>
2013-09-13 01:52:37 +0000

    server thing or something

--- Server/Server.java
+++ Server/Server.java
@@ -1,24 +1,39 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.lang.*;
 
-class Server
+public class Server implements Runnable 
 {
-	public static void main(String argv[]) throws Exception
-	{
-		String clientSentence;
-		ServerSocket welcomeSocket = new ServerSocket(4444);
 
-		while(true)
-		{
-			Socket connectionSocket = welcomeSocket.accept();
-			BufferedReader inFromClient =
-					new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-			clientSentence = inFromClient.readLine();
-			System.out.println("FROM CLIENT: " + clientSentence);
-			outToClient.writeBytes("Connection with server established!\n");
-		}
+	public static void main(String argv[])
+	{
+		Server sv = new Server();
+		sv.run();
+		
 	}
+	
+	public void run()
+	{
+		try 
+		{
+			String clientSentence;
+			ServerSocket welcomeSocket = new ServerSocket(4444);
+
+			while(true)
+			{
+				Socket connectionSocket = welcomeSocket.accept();
+				BufferedReader inFromClient =
+					new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+				DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+				clientSentence = inFromClient.readLine();
+				System.out.println("FROM CLIENT: " + clientSentence);
+				outToClient.writeBytes("Connection with server established!\n");
+			}
+		}
+		catch(IOException e){
+		
+		}
+			
+	}
+
 }
